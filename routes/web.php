9226e9c5d0480c77529b73afb2849e85951ef9fc
Luiz Fernando <87388167+Luferva@users.noreply.github.com>
2024-03-26 13:33:42 -0300

    alterado os layots

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
